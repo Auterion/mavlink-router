e736456d8d9a888c5e78baa99f6559d18a9e43db
Martin Janssen <martin.janssen@auterion.com>
2026-06-05 11:45:11 +0200

    fix: format

--- src/endpoints_test.cpp
+++ src/endpoints_test.cpp
@@ -34,8 +34,9 @@
 // Create custom inhertited class b/c Endpoint can't be initialized containing pure virtual methods
 class TestEndpoint : public Endpoint {
 public:
-  TestEndpoint() : Endpoint{"Test", "foobar"} {};
-  ~TestEndpoint() override {};
+    TestEndpoint()
+        : Endpoint{"Test", "foobar"} {};
+    ~TestEndpoint() override{};    // dummy-implement virtual methods
 
     // dummy-implement virtual methods
     int write_msg(const struct buffer *pbuf) override { return true; };
